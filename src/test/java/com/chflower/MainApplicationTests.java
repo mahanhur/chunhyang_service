1df71efcadf9127dfc4534b56bc2c31e282380cb
youngeunkwon <youngnikk4929@gmail.com>
2023-05-25 14:00:11 +0900

    0525 수정

--- src/test/java/com/chflower/MainApplicationTests.java
+++ src/test/java/com/chflower/MainApplicationTests.java
@@ -4,20 +4,21 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Slf4j
 @SpringBootTest
 class MainApplicationTests {
 
-    @Autowired
-    private BCryptPasswordEncoder encoder;
-
-    @Test
-    void contextLoads() {
-        String rawPassword = "pwd01";
-        String encPassword = encoder.encode(rawPassword);
-        log.info("암호화 된 비번" + encPassword);
-        boolean result = encoder.matches(rawPassword,encPassword);
-        log.info(result+"");
-    }
+//    @Autowired
+//    private BCryptPasswordEncoder encoder;
+//
+//    @Test
+//    void contextLoads() {
+//        String rawPassword = "pwd01";
+//        String encPassword = encoder.encode(rawPassword);
+//        log.info("암호화 된 비번" + encPassword);
+//        boolean result = encoder.matches(rawPassword,encPassword);
+//        log.info(result+"");
+//    }
 }
