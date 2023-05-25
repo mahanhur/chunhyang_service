59be10bc89c1b714398d7365d2b368c34d4ac472
Hayeon001 <khy8723@gmail.com>
2023-05-25 14:06:09 +0900

    0525 14:06

--- src/main/java/com/kbstar/dto/Cust.java
+++ src/main/java/com/kbstar/dto/Cust.java
@@ -2,14 +2,22 @@ package com.kbstar.dto;
 
 import lombok.*;
 
+import java.util.Date;
+
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Setter
 @Getter
 @ToString
 public class Cust {
-    private String id;
-    private String pwd;
-    private String name;
+    private String cust_id;
+    private String cust_name;
+    private String cust_pwd;
+    private String phone;
+    private Number age;
+    private String email;
+    private String cust_del_yn;
+    private Date cust_del_date;
 
 }
