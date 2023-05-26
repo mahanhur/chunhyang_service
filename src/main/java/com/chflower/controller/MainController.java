6d0e67a44e23dced8b7b15c7af6b1c40a81a2e27
Hayeon001 <khy8723@gmail.com>
2023-05-26 14:29:35 +0900

    0526 14:28 index/center분리. 페이지연결

--- src/main/java/com/chflower/controller/MainController.java
+++ src/main/java/com/chflower/controller/MainController.java
@@ -2,17 +2,47 @@ package com.chflower.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
-
-    Logger logger = LoggerFactory.getLogger(this.getClass().getSimpleName());
     @RequestMapping("/")
     public String main(){
         return "index";
     }
 
+    @RequestMapping("/about")
+    public String about(Model model){
+        model.addAttribute("center","about");
+        return "index";
+    }
 
+
+    @RequestMapping("/account-address")  // home > My account > addresses
+    public String account_address(Model model){
+        model.addAttribute("center","account-address");
+        return "index";
+    }
+    @RequestMapping("/account-address-edit")  // home > My account > addresses > 주소수정
+    public String account_address_edit(Model model){
+        model.addAttribute("center","account-address-edit");
+        return "index";
+    }
+    @RequestMapping("/account-order")  // home > My account > addresses > order(단건))
+    public String account_order(Model model){
+        model.addAttribute("center","account-order");
+        return "index";
+    }
+    @RequestMapping("/account-orders")  // home > My account > addresses > orders(주문들))
+    public String account_orders(Model model){
+        model.addAttribute("center","account-orders");
+        return "index";
+    }
+    @RequestMapping("/register")
+    public String register(Model model){
+        model.addAttribute("center","register");
+        return "index";
+    }
 }
