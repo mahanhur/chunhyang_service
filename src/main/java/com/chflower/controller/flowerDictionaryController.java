10b02948dc2b147c964c1dad829a9ae60502a811
Yooseoungjin <yuziyuzi2@gmail.com>
2023-06-14 14:23:52 +0900

    꽃사전 전체조회 추가(일단 16개만 조회) 꽃클릭시 디테일한 내용 추가

--- src/main/java/com/chflower/controller/flowerDictionaryController.java
+++ src/main/java/com/chflower/controller/flowerDictionaryController.java
@@ -7,9 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Slf4j
 @Controller
 @RequestMapping("/flowerdictionary")
