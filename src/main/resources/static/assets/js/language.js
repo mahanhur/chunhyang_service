6f7d00c30d9a9d92c834a91e422967a4c0b076a0
Hayeon001 <khy8723@gmail.com>
2023-06-28 14:50:25 +0900

    0628 오늘의 꽃 다국어 하드코딩

--- src/main/resources/static/assets/js/language.js
+++ src/main/resources/static/assets/js/language.js
@@ -1,13 +1,13 @@
 // 언어별 메뉴 글자 셋팅
 var lang1 = {
     //한국어
-    "ko_KR": ["정기구독", "꽃선물", "화병/비품", "플라워클래스", "꽃사전", "소개", "소개", "로그인", "회원가입", "내정보수정", "로그아웃"],
+    "ko_KR": ["정기구독", "꽃선물", "화병/비품", "플라워클래스", "꽃사전", "소개", "소개"],
     //영어
-    "en_US": ["Subscription", "Flower", "Vase/Furniture", "Class", "Dictionary", "About", "About", "Login", "Register", "My Information", "Logout"],
+    "en_US": ["Subscription", "Flower", "Vase/Furniture", "Class", "Dictionary", "About", "About"],
     //일본어
-    "ja": ["定期購読", "花", "花瓶/備品", "フラワークラス", "花辞典", "紹介", "紹介", "ログイン", "登録", "マイ情報修正", "ログアウト"],
+    "ja": ["定期購読", "花", "花瓶/備品", "フラワークラス", "花辞典", "紹介", "紹介"],
     //중국어
-    "ch": ["定期阅读", "花", "花瓶/备品", "花班", "花词典", "介绍", "介绍", "登录", "注册", "修改我的信息", "注销"],
+    "ch": ["定期阅读", "花", "花瓶/备品", "花班", "花词典", "介绍", "介绍"],
 };
 var lang2 = {
     //한국어
@@ -31,6 +31,25 @@ var lang2 = {
         "today_txt2": ".",
     }
 };
+var lang3 = {
+    //오늘의 꽃 다국어 하드코딩
+    //한국어
+    "ko_KR": {
+        "todayflower_for_lang":".",
+    },
+    //영어
+    "en_US": {
+        "todayflower_for_lang":"French Marigold",
+    },
+    //일본어
+    "ja": {
+        "todayflower_for_lang":"フレンチマリーゴールドです",
+    },
+    //중국어
+    "ch": {
+        "todayflower_for_lang":"法国万寿菊",
+    }
+};
 
 // 함수 셋팅: 언어 선택이 변경될 때 각 요소의 언어로 바꿔줌
 function changeLang(obj) {
@@ -50,4 +69,8 @@ function changeLang(obj) {
     var target2_2 = document.getElementsByClassName("today_txt2")[0]; // today_txt2 요소
     target2_1.innerHTML = languageCode2.today_txt1; // today_txt1 요소에 lang2 값 넣기
     target2_2.innerHTML = languageCode2.today_txt2; // today_txt2 요소에 lang2 값 넣기
+
+    var languageCode3 = lang3[sv]; // lang3 언어 설정 객체 값 가져오기
+    var target3 = document.getElementsByClassName("todayflower_for_lang")[0]; // todayflower_for_lang 요소
+    target3.innerHTML = languageCode3.todayflower_for_lang; // todayflower_for_lang 요소에 lang3 값 넣기
 }
